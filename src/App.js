7aadd4cfe097b837eb77d7fb2a6e43f7d8a820d8
Ahmed Salah <ahmedsz599@gmail.com>
2024-08-04 23:42:42 +0300

    The header part is done.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React , {Component , useState} from 'react';
 import './App.css';
+import Header from './Components/1-header/Header';
+import Hero from './Components/2-hero/Hero';
+import Main from './Components/3-main/Main';
+import Contact from './Components/4-contact/Contact';
+import Footer from './Components/5-footer/Footer';
 
 
 function App() {
 
     return (
-          <div className="App">
-              <h1 className='text-3xl font-bold underline'>Hello Woeld!</h1>
+          <div className="container">
+              <Header/>
+              <div className='divider'/>
+              <Hero />
+              <div className='divider'/>
+              <Main/>
+              <div className='divider'/>
+              <Contact/>
+              <div className='divider'/>
+              <Footer/>
           </div>
     );
   }
